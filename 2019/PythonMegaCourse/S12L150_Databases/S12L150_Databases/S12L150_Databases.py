fd1ec8249bffd05f2709335d1fd26cb3d4192e93
Trent Lynn <trenticle@LIVE.COM>
2019-02-22 09:26:20 -0800

    22-FEB-2019_daily_commit

--- 2019/PythonMegaCourse/S12L150_Databases/S12L150_Databases/S12L150_Databases.py
+++ 2019/PythonMegaCourse/S12L150_Databases/S12L150_Databases/S12L150_Databases.py
@@ -1,3 +1,32 @@
 import sqlite3
+import psycopg2
+
+def create_table():
+    conn=sqlite3.connect('d:\\Users\\trent\\Desktop\\New folder\\lite.db')
+    cur=conn.cursor()
+    cur.execute('CREATE TABLE IF NOT EXISTS store (item TEXT, quantity INTERGER, price REAL)')
+    conn.commit()
+    conn.close()
+
+def insert(item,quantity,price):
+    conn=sqlite3.connect('d:\\Users\\trent\\Desktop\\New folder\\lite.db')
+    cur=conn.cursor()
+    cur.execute('INSERT INTO store VALUES (?,?,?)',(item,quantity,price))
+    conn.commit()
+    conn.close()
+
+insert('coffee cup',10,5)
+
+def view():
+    conn=sqlite3.connect('d:\\Users\\trent\\Desktop\\New folder\\lite.db')
+    cur=conn.cursor()
+    cur.execute('SELECT * FROM store')
+    rows=cur.fetchall()
+    conn.close()
+    return rows
+
+print(view())
+
+
 
 
