1895ad312e44dd0991c6e6228d82d6080de94ffd
trenticle <42152693+trenticle@users.noreply.github.com>
2019-08-11 21:17:44 -0700

    11-AUG-2019 housekeeping commit

--- 2019/PythonMegaCourse/S11L148_Create_A_GUI/S11L148_Create_A_GUI/S11L148_Create_A_GUI.py
+++ 2019/PythonMegaCourse/S11L148_Create_A_GUI/S11L148_Create_A_GUI/S11L148_Create_A_GUI.py
@@ -1,7 +1,7 @@
 from tkinter import *
-   
+
 window=Tk()
-     
+
 def from_kg():
     gram=float(e2_value.get())*1000
     pound=float(e2_value.get())*2.20462
@@ -12,24 +12,33 @@ def from_kg():
     t2.insert(END,pound)
     t3.delete("1.0", END)
     t3.insert(END,ounce)
-     
+
 e1=Label(window,text="Kg")
 e1.grid(row=0,column=0)
-     
+
 e2_value=StringVar()
 e2=Entry(window,textvariable=e2_value)
 e2.grid(row=0,column=1)
-     
+
 b1=Button(window,text="Convert",command=from_kg)
 b1.grid(row=0,column=2)
-     
-t1=Text(window,height=1,width=20)
+
+t1=Label(window,text="gram")
 t1.grid(row=1,column=0)
-     
+
+t1=Text(window,height=1,width=20)
+t1.grid(row=1,column=1)
+
+t2=Label(window,text="Lb")
+t2.grid(row=1,column=2)
+
 t2=Text(window,height=1,width=20)
-t2.grid(row=1,column=1)
-     
+t2.grid(row=1,column=3)
+
+t3=Label(window,text="oz")
+t3.grid(row=1,column=4)
+
 t3=Text(window,height=1,width=20)
-t3.grid(row=1,column=2)
-     
+t3.grid(row=1,column=5)
+
 window.mainloop()
--- 2019/PythonMegaCourse/S12L150_Databases/S12L150_Databases/S12L150_Databases.py
+++ 2019/PythonMegaCourse/S12L150_Databases/S12L150_Databases/S12L150_Databases.py
@@ -1,3 +1,44 @@
 import sqlite3
+import psycopg2
 
+def create_table():
+    conn=sqlite3.connect('c:\\users\\trent\\desktop\\lite.db')
+    cur=conn.cursor()
+    cur.execute('CREATE TABLE IF NOT EXISTS store (item TEXT, quantity INTERGER, price REAL)')
+    conn.commit()
+    conn.close()
 
+def insert(item, quantity, price):
+    conn=sqlite3.connect('c:\\users\\trent\\desktop\\lite.db')
+    cur=conn.cursor()
+    cur.execute("INSERT INTO store VALUES (?, ?, ?)", (item, quantity, price))
+    conn.commit()
+    conn.close()
+
+# insert('Coffee Cup', 10, 5)
+
+def view():
+    conn=sqlite3.connect('c:\\users\\trent\\desktop\\lite.db')
+    cur=conn.cursor()
+    cur.execute('SELECT * FROM store')
+    rows=cur.fetchall()
+    conn.close()
+    return rows
+
+def update(quantity, price, item):
+    conn=sqlite3.connect('c:\\users\\trent\\desktop\\lite.db')
+    cur=conn.cursor()
+    cur.execute('UPDATE store SET quantity=?, price=? WHERE item=?', (quantity, price, item))
+    conn.commit()
+    conn.close()
+
+def delete(item):
+    conn=sqlite3.connect('c:\\users\\trent\\desktop\\lite.db')
+    cur=conn.cursor()
+    cur.execute('DELETE FROM store WHERE item=?', (item,))
+    conn.commit()
+    conn.close()
+
+update(11, 6, "Water Glass")
+# delete('Wine Glass')
+print(view())
