3ad0ae896aecfb9eb2b05229096c6a9590a9705b
Trent Lynn <trenticle@LIVE.COM>
2020-07-28 20:55:04 -0700

    Daily Commit 28-JUL-2020

--- 2020/PMC S10L67 More Functions/PMC S10L67 More Functions/PMC_S10L67_More_Functions.py
+++ 2020/PMC S10L67 More Functions/PMC S10L67 More Functions/PMC_S10L67_More_Functions.py
@@ -1,17 +1,47 @@
-def area (a, b):
-    return a*b
-print(area(4, 5))
+#def area (a, b):
+    #return a*b
+#print(area(4, 5))
 
 # shit exercise not well defined, how can you print this garbage? return s1 + s2, no space? crap! yeah, checks out, it's pure garbage.
-def foo(s1, s2):
-    return s1 + s2
-s1 = "me"
-s2 = "too"
+#def foo(s1, s2):
+    #return s1 + s2
+#s1 = "me"
+#s2 = "too"
 
-print(foo(s1, s2))
+#print(foo(s1, s2))
 
 # 2 kinds of arguements: keyword and non-keyword arguements
-# default perameters (b=6) in definition
+ #default perameters (b=6) in definition
 # non-default (variable) must go first in list before default(fixed value) perameters
+
+#def mean(*args):
+    #return sum(args) / len(args)
+
+#print(mean(1, 4, x=3))
+
+#exercise
+#wrong answer
+foo = (10, 20, 30, 40)
+#def mean(*foo):
+    #return sum(foo) / len(foo)
+#print(mean(foo))
+
+#right answer
+#def foo(*args):
+    #return sum(args) / len(args)
+#print(foo())
+
+#def foo(*args):
+    #args = [x.upper() for x in args]
+    #return sorted(args)
+
+#def mean(**kwargs):
+    #return kwargs
+#print (mean(a=1, b=2, c=3))
+
+def find_sum(**kwargs):
+    return sum(kwargs.values())
+print(find_sum(x=9))
+
 
 
---
+++ 2020/PMC S11L73 File Processing/PMC S11L73 File Processing/PMC_S11L73_File_Processing.py
@@ -0,0 +1,25 @@
+from pathlib import Path
+
+myfile = open('d:\\users\\trent\\desktop\\sandbox\\fruits.txt')
+content = myfile.read()
+print(content)
+print(content)
+
+#close file
+myfile.close()
+
+#open and close
+with open('d:\\users\\trent\\desktop\\sandbox\\fruits.txt') as myfile:
+    content = myfile.read()
+print(content)
+
+#Path library opens AND closes the file
+myfile = Path('d:\\users\\trent\\desktop\\sandbox\\fruits.txt')
+print(myfile.read_text())
+
+#write to mode='w'
+with open('d:\\users\\trent\\desktop\\sandbox\\vegatables.txt', 'w') as myOtherFile:
+    myOtherFile.write("Tomato")
+
+
+
